b2d0233bdbbcc44c902dd8553cd4498977603d31
Alex4J <toacoin@gmail.com>
2017-10-19 23:32:47 +0200

    First commit

---
+++ src/parameters.h
@@ -0,0 +1,25 @@
+#ifndef BITCOIN_PARAMETERS
+#define BITCOIN_PARAMETERS
+
+#define PREMINED 500000000
+#define MAX_COIN 600000000
+#define INFLATION 0.03
+#define MINIMUM_STAKE 25000
+#define DIVIDE 25000
+#define RPC_PORT 41575
+#define STR_RPC_PORT "41575"
+#define PORT 94376
+#define STR_PORT "94376"
+#define POW_REWARD 0
+#define DEF_pchMessageStart0 0x06
+#define DEF_pchMessageStart1 0x8a
+#define DEF_pchMessageStart2 0xda
+#define DEF_pchMessageStart3 0xee
+#define DEF_PUBKEY_ADDRESS 73
+#define DEF_nStakeCombineThreshold 100000
+
+#define GENESIS_PSZ "Calvin Ayre Declares Bitcoin Cash “The Only Bitcoin”"
+#define GENESIS_BLK "0x0000000286d17c7a636168d186258a5fd6b1f572f29cfd32365fa4601ae7f091"
+#define GENESIS_TIME 1508435994
+
+#endif
